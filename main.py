2e7452fb993cf33b1e258b9fc63b11e8c57bf03a
rajko <rajkonkret660@gmail.com>
2023-11-18 12:26:49 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# ctrl shift f10 - uruchamianie programu
+# Uruchomienie programu przez linie komend
+# (venv) PS C:\Users\rajko\PycharmProjects\bootcamp-python-18-11> python.exe .\main.py
+# Hi, PyCharm
+# ctrl / - automatyczne dodanie komentarza
+# ctrl alt l  - formatowanie tekstu wg zasad PEP8
