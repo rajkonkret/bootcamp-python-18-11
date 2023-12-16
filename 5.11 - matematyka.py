46d5ab84879cabdbc12bae7a6c516731cc67ef90
rajkonkret <rajkonkret660@gmail.com>
2023-12-16 14:46:33 +0100

    5 - update matematyka

--- 5.11 - matematyka.py
+++ 5.11 - matematyka.py
@@ -23,5 +23,19 @@ x = math.ceil(1.4)  # zaorągla w góre
 y = math.floor(1.4)  # zaofkrągla w dół
 print(x, y)  # 2 1
 
-x = math.cos(90)
+x = math.cos(90)  # podajemy w radianach
 print(x)
+# zamiana kąta na radiany
+radian = math.radians(180)
+print(radian)
+x = math.cos(3.141592653589793)
+print(x)  # -1.0
+
+x = math.exp(2)
+print(f"e ^ 2 = {x}")  # e ^ 2 = 7.38905609893065
+
+log_val = math.log(10)
+print(f"Log: {log_val}")
+
+log10_val = math.log10(10)
+print(f"Log10: {log10_val}")  # Log10: 1.0
