9c2b79efa90e5803ef04446587161ff402691e94
rajkonkret <rajkonkret660@gmail.com>
2023-12-16 14:37:38 +0100

    5 - add matematyka

---
+++ 5.11 - matematyka.py
@@ -0,0 +1,27 @@
+# math
+import math
+
+x = min(5, 10, 20)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+x = abs(-7.25)
+print(x)
+
+x = pow(4, 3)  # potęgowanie
+print(x)
+print(4 ** 3)
+
+print(math.pi)  # 3.141592653589793
+
+x = math.sqrt(64)
+print(x)  # 8.0 - float
+
+x = math.ceil(1.4)  # zaorągla w góre
+y = math.floor(1.4)  # zaofkrągla w dół
+print(x, y)  # 2 1
+
+x = math.cos(90)
+print(x)
