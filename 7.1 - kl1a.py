11e966d842eae03fe12e36d66520e73c8bda845a
rajkonkret <rajkonkret660@gmail.com>
2024-01-13 09:57:32 +0100

    7 - add kl1a

---
+++ 7.1 - kl1a.py
@@ -0,0 +1,64 @@
+import math  # biblioteka do działań matematycznych
+
+
+class MyFirstClass:
+    """
+    Klasa w Pythonie, opisująca punkty w przestrzeni x i y
+    """
+
+    def __init__(self, x=0, y=0):
+        """
+        funkcja inicjalizująca
+        :param x:
+        :param y:
+        """
+        # self.x = x
+        # self.y = y
+        # Po dodaniu funkcji move()
+        self.move(x, y)  # wywołanie funkcji move()
+
+    def move(self, x: float, y: float) -> None:
+        """
+        funkcja przesuwa punkt w dane miejsce
+        :param x:
+        :param y:
+        :return: None
+        """
+        self.x = x
+        self.y = y
+
+    def reset(self) -> None:
+        """
+        funkcja wraca punkt do miejsca 0,0
+        :return: None
+        """
+        self.move(0, 0)
+
+    # funkcja odpowiada za sposob w jaki jest wyswietlany obiekt
+    def __str__(self):
+        return f"({self.x, self.y})"
+
+    # reprezentacja obiektu
+    def __repr__(self):
+        return f"Point({self.x, self.y})"
+
+
+ob = MyFirstClass()  # <__main__.MyFirstClass object at 0x104666590>
+# po dodaniu funkcji __str__ opis wygląda tak: ((0, 0))
+print(ob)
+print(ob.x)
+print(ob.y)
+ob.move(4, 5)
+print(ob)  # ((4, 5))
+ob.reset()
+print(ob)  # ((0, 0))
+ob2 = MyFirstClass(5, 7)
+print(ob2)  # ((5, 7))
+
+lista_punkty = [ob, ob2]
+print(lista_punkty)  # [<__main__.MyFirstClass object at 0x100716f50>, <__main__.MyFirstClass object at 0x100716f90>]
+# po dodaniu __repr__
+# [Point((0, 0)), Point((5, 7))]
+ob_str = str(ob)  # uzywa __str__
+lista2 = [ob_str]
+print(lista2)  # ['((0, 0))']
