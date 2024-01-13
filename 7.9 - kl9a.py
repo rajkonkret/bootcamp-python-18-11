75e5b5de64a63ef2f867115f43bbacb765c7ed66
rajkonkret <rajkonkret660@gmail.com>
2024-01-13 14:48:00 +0100

    7 - add kl9a

---
+++ 7.9 - kl9a.py
@@ -0,0 +1,34 @@
+class PrintedMedia:
+    def print_media(self):
+        print("Drukowana treść")
+
+
+class DigitalMedia:
+    def digital_media(self):
+        print("Cyfrowa treść")
+
+
+class Book(PrintedMedia):
+    pass
+
+
+class Ebook(DigitalMedia):
+    pass
+
+
+class MultimediaBook(Book, Ebook):
+    pass
+
+
+mulimedia_ebook = MultimediaBook()
+print(MultimediaBook.__mro__)
+# (<class '__main__.MultimediaBook'>,
+# <class '__main__.Book'>,
+# <class '__main__.PrintedMedia'>,
+# <class '__main__.Ebook'>,
+# <class '__main__.DigitalMedia'>,
+# <class 'object'>)
+# mulimedia_ebook.print_media()
+# mulimedia_ebook.digital_media()
+# Drukowana treść
+# Cyfrowa treść
