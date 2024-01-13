438001136ed528c2f31719587087872080e527f3
rajkonkret <rajkonkret660@gmail.com>
2024-01-13 13:26:01 +0100

    7 - update kl4a

--- 7.4 - kl4a - missing.py
+++ 7.4 - kl4a - missing.py
@@ -5,7 +5,16 @@ class DefaultDict(dict):
         return "default"
 
 
+class AutoKeyDict(dict):
+    def __missing__(self, key):
+        self[key] = 0
+        return key
 
+
+class CaseInsesitiveDict(dict):
+    def __missing__(self, key):
+        if isinstance(key, str):
+            return self.get(key.lower())
 
 
 d_python = {}  # pusty słownik
@@ -16,3 +25,16 @@ print(d_python['name'])  # Radek
 d1 = DefaultDict()
 print(d1['name'])  # default - nadpisana metoda __missing__
 # zrobic własny słownik, który gdy nie ma klucza w słowniku doda go do słownika z domyslną wartością
+
+d2 = AutoKeyDict()
+print(d2)
+print(d2['name'])  # name
+print(d2)  # {'name': 0}
+d2['name'] = "Radek"
+print(d2['name'])  # Radek
+
+# słownik, który potrafi korzystac z klucza niezależnie czy zostanie podany jako małe czy duże litery
+# print(1.lower())  # SyntaxError: invalid decimal literal
+d3 = CaseInsesitiveDict()
+d3['name'] = 'Radek'
+print(d3['Name'])  # Radek
