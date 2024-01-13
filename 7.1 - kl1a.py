22002f7affe6886a4ac56834911552e0dff6f46a
rajkonkret <rajkonkret660@gmail.com>
2024-01-13 10:31:32 +0100

    7 - update kl1a

--- 7.1 - kl1a.py
+++ 7.1 - kl1a.py
@@ -34,6 +34,9 @@ class MyFirstClass:
         """
         self.move(0, 0)
 
+    def calculate(self, other: "MyFirstClass") -> float:
+        return math.hypot(self.x - other.x, self.y - other.y)
+
     # funkcja odpowiada za sposob w jaki jest wyswietlany obiekt
     def __str__(self):
         return f"({self.x, self.y})"
@@ -62,3 +65,8 @@ print(lista_punkty)  # [<__main__.MyFirstClass object at 0x100716f50>, <__main__
 ob_str = str(ob)  # uzywa __str__
 lista2 = [ob_str]
 print(lista2)  # ['((0, 0))']
+print(ob.calculate(ob2))  # 8.602325267042627
+# Używamy hintów (podpowiedzi)
+# one nie weryfikuja typu
+# ob.calculate("a")  # AttributeError: 'str' object has no attribute 'x'
+# stworzyć dwa obiekty i obliczyc odległość miedzy nimi
