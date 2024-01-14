7b7673e4342a1ffd64f5aee6a319a3209e972491
rajkonkret <rajkonkret660@gmail.com>
2024-01-14 12:31:48 +0100

    8 - add kl21

---
+++ 8.12 - kl21a.py
@@ -0,0 +1,31 @@
+class MyNumber:
+    def __init__(self, value):
+        self.value = value
+
+
+num1 = MyNumber(5)
+num2 = MyNumber(15)
+print(5 < 15)  # True
+# print(num1 < num2)  # TypeError: '<' not supported between instances of 'MyNumber' and 'MyNumber'
+# brak metody __lt__
+print(num1.value < num2.value)  # True
+
+
+class MyNumber2:
+    def __init__(self, value):
+        self.value = value
+
+    def __lt__(self, other):
+        """Porównuje obiekty po wartości pola value"""
+        return self.value < other.value
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+
+num3 = MyNumber2(5)
+num4 = MyNumber2(15)
+num5 = MyNumber2(15)
+print(num3 < num4)  # True
+print(num4 == num5)  # False, porównał referencje
+# po dopisaniu metody __eq__ bedzie True
