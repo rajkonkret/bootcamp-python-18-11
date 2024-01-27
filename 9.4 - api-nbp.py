bba2997ef03bb34e36c9e8379412990027f66429
rajkonkret <rajkonkret660@gmail.com>
2024-01-27 11:58:37 +0100

    9 - update api-nbp

--- 9.4 - api-nbp.py
+++ 9.4 - api-nbp.py
@@ -1,4 +1,6 @@
 # http://api.nbp.pl/api/exchangerates/rates/{table}/{code}/
+# Komunikacja z serwisem polega na wysłaniu odpowiednio sparametryzowanego żądania HTTP GET
+# na adres bazowy http://api.nbp.pl/api/.
 # Parametry zapytań o kursy walut
 #
 # {table} – typ tabeli (A, B, lub C)
@@ -6,4 +8,24 @@
 # {topCount} – liczba określająca maksymalną liczność zwracanej serii danych
 # {date}, {startDate}, {endDate} – data w formacie RRRR-MM-DD (standard ISO 8601)
 # oczekuje informacji jaki jest dzis kurs euro i dolara
+# http://api.nbp.pl/api/exchangerates/rates/{table}/{code}/
+# {table} za tekst z klamerkami wstawiamy wartość u nas: A
+# {code} za tekst z klamerkami wstawiamy wartość u nas: EUR
+
+import requests as re
+
+url = 'http://api.nbp.pl/api/exchangerates/rates/A/EUR/'
+response = re.get(url)
+print(response)  # <Response [200]>
+table = response.json()
+print(table)
+# {'table': 'A', 'currency': 'euro', 'code': 'EUR',
+#  'rates': [{'no': '019/A/NBP/2024', 'effectiveDate': '2024-01-26', 'mid': 4.3802}]}
+print(table['rates'][0]['mid'])  # 4.3802
+# print(table['rates']
+# -> lista (wyciagamy pierwszy element)[0]
+# -> słownik (wyciągamy po kluczu)['mid'])  # 4.3802
+
+# zamienic na deserializacje na obiekty
+
 
