171dac7e23255ce643e1ab2ccd82f19b1536eef1
rajkonkret <rajkonkret660@gmail.com>
2024-01-27 11:45:33 +0100

    9 - add api-nbp

---
+++ 9.4 - api-nbp.py
@@ -0,0 +1,9 @@
+# http://api.nbp.pl/api/exchangerates/rates/{table}/{code}/
+# Parametry zapytań o kursy walut
+#
+# {table} – typ tabeli (A, B, lub C)
+# {code} – trzyliterowy kod waluty (standard ISO 4217)  USD, EUR
+# {topCount} – liczba określająca maksymalną liczność zwracanej serii danych
+# {date}, {startDate}, {endDate} – data w formacie RRRR-MM-DD (standard ISO 8601)
+# oczekuje informacji jaki jest dzis kurs euro i dolara
+
