f0eb0bd176ea84e7be4a9fc3bca1d131b3e1a08b
rajkonkret <rajkonkret660@gmail.com>
2024-01-27 10:17:03 +0100

    9 - update api randomuser

--- 9.2 - api-randouser.py
+++ 9.2 - api-randouser.py
@@ -1,4 +1,6 @@
 import requests as re
+from pydantic import BaseModel  # serializacja danych typu json, bazy danych , podobne
+from typing import List
 
 url = 'https://randomuser.me/api/'
 response = re.get(url)
@@ -37,4 +39,43 @@ with open('user_photo.jpg', 'wb') as f:
     f.write(response_photo.content)
 
 print("Zdjęcie zostało zapisane")
+
+
 # zbudowac schemat klas dla name(first,last), email, picture(large)
+
+class UserInfo(BaseModel):
+    name: dict
+    email: str
+    picture: dict
+
+
+# user = data['results'][0]  - wyciągneliśmy wewnętrznego jsona z listy w jakiej w jsonie był
+# parsujemy tylko ten wewnętrzny
+print(user)
+# {'gender': 'male', 'name': {'title': 'Mr', 'first': 'Prebislav', 'last': 'Brovchenko'},
+#  'location': {'street': {'number': 295, 'name': 'Akademika Iefremova'},
+#  'city': 'Yagotin', 'state': 'Kirovogradska',
+#  'country': 'Ukraine',
+#  'postcode': 95974,
+#  'coordinates': {'latitude': '75.4885', 'longitude': '-132.6496'},
+#               'timezone': {'offset': '+3:00', 'description': 'Baghdad, Riyadh, Moscow, St. Petersburg'}},
+#  'email': 'prebislav.brovchenko@example.com',
+#  'login': {'uuid': 'a1805f6f-ff89-4b94-9dde-db2b3e46e19d', 'username': 'ticklishswan876', 'password': 'soccer1',
+#            'salt': '07FJTLpM', 'md5': 'e45a6ea02e44e7288624bc58a0f56274',
+#            'sha1': '81fadfb7b73f36984495c4b49a60f8555043b874',
+#            'sha256': '9bb14c43022a91e19f545a778493d8f50e38cdcd8cc4207426254798aad1d078'},
+#  'dob': {'date': '1986-07-16T11:23:20.963Z', 'age': 37}, 'registered': {'date': '2005-11-02T04:44:38.680Z', 'age': 18},
+#  'phone': '(067) Y24-8068', 'cell': '(066) X25-5320', 'id': {'name': '', 'value': None},
+#  'picture': {'large': 'https://randomuser.me/api/portraits/men/74.jpg',
+#              'medium': 'https://randomuser.me/api/portraits/med/men/74.jpg',
+#              'thumbnail': 'https://randomuser.me/api/portraits/thumb/men/74.jpg'}, 'nat': 'UA'}
+
+# user = data['results'][0]  - wyciągneliśmy wewnętrznego jsona z listy w jakiej w jsonie był
+user_info = UserInfo(**user)  # ** rozpakowanie danych słownikowych na obiekt wskazanej klasy
+print(type(user_info))  # user = data['results'][0]  - wyciągneliśmy wewnętrznego jsona z listy w jakiej w jsonie był
+print(user_info)
+# name = {'title': 'Mr', 'first': 'Prebislav', 'last': 'Brovchenko'}
+# email = 'prebislav.brovchenko@example.com'
+# picture = {'large': 'https://randomuser.me/api/portraits/men/74.jpg',
+#            'medium': 'https://randomuser.me/api/portraits/med/men/74.jpg',
+#            'thumbnail': 'https://randomuser.me/api/portraits/thumb/men/74.jpg'}
