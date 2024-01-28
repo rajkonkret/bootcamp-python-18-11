175c5e8f0af00efe73b216f504bd01741c24dd30
rajkonkret <rajkonkret660@gmail.com>
2024-01-28 11:17:55 +0100

    10 - add baza2

--- 10.3 - baza2.py
+++ 10.3 - baza2.py
@@ -5,6 +5,20 @@ try:
     # sql_connection = sqlite3.connect(':memory:')  # baza danych w pamięci
     cursor = sql_connection.cursor()
     print("Baza danych została podłączona")
+
+    query = '''
+    CREATE TABLE SqliteDB_developers (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    joining_date DATETIME,
+    salary REAL NOT NULL
+    );'''
+
+    cursor.execute(query)  # wykonanie polecenia sql(query) na bazie danych
+    sql_connection.commit()  # utrwalenie zmian w bazie
+
+
 except sqlite3.Error as e:
     print("Błąd bazy danych", e)
 finally:
