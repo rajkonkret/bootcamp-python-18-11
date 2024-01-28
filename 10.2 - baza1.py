a775ecf520ac8c8f8b15e1d7aeb1f1d6f8ad708d
rajkonkret <rajkonkret660@gmail.com>
2024-01-28 10:40:09 +0100

    10 - ad baza1

---
+++ 10.2 - baza1.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+# sqlite3 test.db  - otworzenie bazy, stworzy nową gdy nie ma
+# https://www.heidisql.com
+# https://sqlitebrowser.org/dl/
+# tworzenie tabeli w bazie danych (sql)
+# create table users (id INT PRIMARY KEY, name VARCHAR(100), age INT);
+# dodanie uzytkownika do tabeli (sql)
+# insert into users (id, name, age) values (1,'Jan Kowalski', 30);
+# pokazuje tabele w bazie danych za pomoca silnika sqlite3
+# sqlite> .tables  - wyswietla liste tabel
+# users
+# odczyt danych z tabeli
+# select * from users; (sql)
+# 1|Jan Kowalski|30
+# zmiana danych rekordu
+# update users set age=31 where id=1; - zmiana age na 31 dla rekordu o id=1
+# usunięcie rekordu (wpisu)
+#  delete from users where id=1; - usunięcie elementu o id=1
+# wypisanie zawartości kolumnu name z tabeli users
+# select name from users;
+# wypisz posortowane wg age - kolejnosc rosnąca
+# select * from users order by age;
+# wypisz posortowane, kolejnosc malejąca - desc
+# select * from users order by age desc;
+# wypisanie tylko takich, gdzie wiek > 25
+# select * from users where age > 25;
+# importowanie danych do bazy z pliku csv
+# sqlite> create table users(id INT, name TEXT, emali TEXT, age INT);
+# sqlite> .mode csv
+# sqlite> .import dane_person.csv users
+# sqlite>
