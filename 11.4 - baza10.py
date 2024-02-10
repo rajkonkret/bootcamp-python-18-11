e6b2c7d916be40ab0993f9d2a03c8ffa43cc6aef
rajkonkret <rajkonkret660@gmail.com>
2024-02-10 14:00:10 +0100

    11 - add baza 10

--- 11.4 - baza10.py
+++ 11.4 - baza10.py
@@ -36,12 +36,41 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-new_author = Author(name='Adam Mickiewicz')
-new_publisher = Publisher(name='Wydawnictwo XYZ')
-new_book = Book(title="Pan Tadeusz", author=new_author, publisher=new_publisher)
+# new_author = Author(name='Adam Mickiewicz')
+# new_publisher = Publisher(name='Wydawnictwo XYZ')
+# new_book = Book(title="Pan Tadeusz", author=new_author, publisher=new_publisher)
+#
+# session.add_all(
+#     [new_author, new_publisher, new_book]
+# )
+new_author = Author(name='Jan Kowalski')
+new_publisher = Publisher(name='Wydawnictwo ABC')
+new_book = Book(title="Nauka Pythona od podstaw", author=new_author, publisher=new_publisher)
 
 session.add_all(
     [new_author, new_publisher, new_book]
 )
 
 session.commit()
+# odczytac z bazy autorów i z autorów odczytać ich ksiązki
+# z ksiązęk odczytać wydawców
+# wypisać autor, ksiązka, wydawca
+
+authors = session.query(Author).all()
+print(authors)
+for author in authors:
+    print(f"Author: {author.name}")
+    for b in author.books:
+        # print(b)
+        print(f"Ksiązka: {b.title}, wydawca: {b.publisher.name}")  # Ksiązka: Pan Tadeusz, wydawca: Wydawnictwo XY
+        print(f"Ksiązka: {b.title}, wydawca: {b.publisher}")
+    # Ksiązka: Pan Tadeusz, wydawca: <__main__.Publisher object at 0x10514a5a0>
+# Author: Adam Mickiewicz
+# Ksiązka: Pan Tadeusz, wydawca: Wydawnictwo XYZ
+
+# wypisac wydawców i ich ksiazki
+publishers = session.query(Publisher).all()
+for publisher in publishers:
+    print(f"Wydawca: {publisher.name}")
+    for book in publisher.books:
+        print(f"Ksiązka: {book.title}")
