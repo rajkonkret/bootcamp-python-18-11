ded5a2b02cf5b3cf81c247c3f985d8e91566f6ab
rajkonkret <rajkonkret660@gmail.com>
2024-02-10 08:54:08 +0100

    11 - update praca domowa

--- 11.1 - api-numbers.py
+++ 11.1 - api-numbers.py
@@ -9,5 +9,10 @@ print(type(data))  # <class 'dict'>
 # {'text': '699 is the year that Umayyad troops invade Armenia and secure submission of Smbat VI
 # Bagratuni.',
 #  'number': 699, 'found': True, 'type': 'year'}
-print("Podaj odpowiedź na pytanie:\n", data['text'])
+print("Podaj odpowiedź na pytanie:\n", data['text'].replace(str(data['number']), ""))
+odp = input()  # str
+if odp == str(data['number']):  # data['number'] -> int
+    print("Prawidłowo")
+
 print(data['number'])
+# regex - wyrażenia regularne
