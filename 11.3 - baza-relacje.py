e5c4b89635352c18dcb609bce9eb4391d421640e
rajkonkret <rajkonkret660@gmail.com>
2024-02-10 11:36:39 +0100

    11 - update baza relacje

--- 11.3 - baza-relacje.py
+++ 11.3 - baza-relacje.py
@@ -37,7 +37,7 @@ class Person(Base):
     )
 
     def __repr__(self):
-        return f"{self.name} (id={self.id})"
+        return f"{self.name} (id={self.id})"  # Anakin(id=2)
 
 
 class Address(Base):
@@ -50,7 +50,7 @@ class Address(Base):
     def __str__(self):
         return self.email
 
-    __repr__ = __str__
+    __repr__ = __str__  # a = 7
 
 
 Base.metadata.create_all(engine)
@@ -61,7 +61,50 @@ session = Session()
 anakin = Person(name='Anakin', age='38')
 anakin2 = Person(name='Anakin Akakin', age=38)
 anakin2.addresses = [Address(email='anakinkakin@wp.pl')]
+obi = Person(name="Obi Wan Kenobi", age=45)
+obi.addresses = [
+    Address(email='obi@example.com'),
+    Address(email='waaka@example.com')
+]
 
 session.add(anakin)
 session.add(anakin2)
+session.add(obi)
+
 session.commit()
+
+all_ = session.query(Person).all()
+print(all_)
+# [Anakin(id=1), Anakin(id=2), Anakin Akakin(id=3), Anakin(id=4), Anakin Akakin(id=5), Obi Wan Kenobi(id=6), Anakin(id=7),
+#  Anakin Akakin(id=8), Obi Wan Kenobi(id=9)]
+
+an1 = session.query(Person).first()
+print(an1)  # Anakin (id=1)
+print(type(an1))  # <class '__main__.Person'>
+print(an1.name, an1.age)  # Anakin 38
+
+obi_list = session.query(Person).filter(
+    Person.name.like('Obi%')
+).all()
+
+print(obi_list)
+# [Obi Wan Kenobi (id=6),
+#  Obi Wan Kenobi (id=9),
+#  Obi Wan Kenobi (id=12),
+#  Obi Wan Kenobi (id=15),
+#  Obi Wan Kenobi (id=18),
+#  Obi Wan Kenobi (id=21)]
+
+# wyswietlic name, age, address
+for o in obi_list:
+    print("id=", o.id, 'name:', o.name, "wiek:", o.age, "email:", o.addresses)
+# id= 24 name: Obi Wan Kenobi wiek: 45 email: [obi@example.com, waaka@example.com]
+# id= 6 name: Obi Wan Kenobi wiek: 45 email: [obi@example.com, waaka@example.com]
+# id= 9 name: Obi Wan Kenobi wiek: 45 email: [obi@example.com, waaka@example.com]
+# id= 12 name: Obi Wan Kenobi wiek: 45 email: [obi@example.com, waaka@example.com]
+# id= 15 name: Obi Wan Kenobi wiek: 45 email: [obi@example.com, waaka@example.com]
+# id= 18 name: Obi Wan Kenobi wiek: 45 email: [obi@example.com, waaka@example.com]
+# id= 21 name: Obi Wan Kenobi wiek: 45 email: [obi@example.com, waaka@example.com]
+# id= 24 name: Obi Wan Kenobi wiek: 45 email: [obi@example.com, waaka@example.com]
+# id= 27 name: Obi Wan Kenobi wiek: 45 email: [obi@example.com, waaka@example.com]
+# id= 30 name: Obi Wan Kenobi wiek: 45 email: [obi@example.com, waaka@example.com]
