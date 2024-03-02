6206377d0e249470be9cc11b8020d77049e65626
rajkonkret <rajkonkret660@gmail.com>
2024-03-02 15:00:30 +0100

    13 - add templates and jinja

---
+++ web_app_with_tempaltes/flask_project.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return 'This is index'
+
+
+@app.route("/exchange", methods=['GET', 'POST'])
+def exchange():
+    if request.method == 'GET':
+        return render_template('exchange.html')
+    else:
+        currency = "EUR"
+        if 'currency' in request.form:
+            currency = request.form['currency']
+
+        amount = "EUR"
+        if 'amount' in request.form:
+            amount = request.form['amount']
+
+        return render_template('exchange_results.html', currency=currency, amount=amount)
+
+if __name__ == '__main__':
+    app.run(debug=True)
