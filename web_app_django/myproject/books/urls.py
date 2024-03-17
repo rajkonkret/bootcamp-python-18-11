68ef2f82fff6d17b65fcf581a65f53a2d7007d82
rajkonkret <rajkonkret660@gmail.com>
2024-03-17 14:58:38 +0100

    16 - add create books

--- web_app_django/myproject/books/urls.py
+++ web_app_django/myproject/books/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import book_list
+from .views import book_create
+
 # tu jest defincja endpointów (routów) - '' głowny, bedzie wyswietlał dane z metody book_list zawartej w views
 urlpatterns = [
-    path('', book_list, name='book_list')
+    path('', book_list, name='book_list'),
+    path('new/', book_create, name='book_create'),
 ]
--- web_app_django/myproject/books/views.py
+++ web_app_django/myproject/books/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book
+from .forms import BookForm
 
 
 # Create your views here.
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'books/book_list.html', {'books': books})
+
+
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+        return render(request, 'books/book_form.html', {'form': form})
